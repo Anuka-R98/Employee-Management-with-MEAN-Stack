865def38c23ca5d84783f5c549f198960d60c2eb
Anuka <arrinsamurai7@gmail.com>
2023-01-17 20:32:45 +0530

    controller/model

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const mongoose = require('./db.js');
+const { mongoose } = require('./db.js');
+var employeeController = require('./controllers/employee_controller');
 
 var app = express();
 app.use(bodyParser.json());
 
-app.listen(3000, ()=> console.log('Server started at port : 3000'));
+app.listen(3000, ()=> console.log('Server started at port : 3000'));    
+
+app.use('/employees', employeeController);
