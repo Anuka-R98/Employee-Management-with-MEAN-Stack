aaec73307df3afd4de05f794285ce84771a91353
Anuka <arrinsamurai7@gmail.com>
2023-01-18 12:28:52 +0530

    front end

---
+++ AngularApp/src/app/employee/employee.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms'
+
+import { EmployeeService } from '../shared/employee.service'
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+  providers: [ EmployeeService ]
+})
+
+export class EmployeeComponent {
+
+  constructor( protected employeeService: EmployeeService ) {}
+
+  ngOnInit(): void {
+    this.resetForm();
+  }
+
+  resetForm( form?: NgForm ) {
+    if (form)
+      form.reset();
+    this.employeeService.selectedEmployee = {
+      _id: "",
+      name: "",
+      position: "",
+      office: "",
+      salary: null  as any
+    }
+  }
+
+}
---
+++ AngularApp/src/app/shared/employee.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/toPromise';
+
+import { Employee } from './employee.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class EmployeeService {
+  
+  selectedEmployee: Employee = new Employee;
+  employees: Employee[] = [];
+
+  constructor() { }
+}
